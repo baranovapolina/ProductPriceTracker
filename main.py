03697176e736829fe10855f7c070c4237e7871ee
Polina Baranova <emailforeducation1@gmail.com>
2025-03-26 17:09:36 +0200

    Додано метод read_file

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ class ProductPriceTracker:
         self.file_path = file_path
         self.data = self.read_file()
 
+     def read_file(self):
+        """
+        Зчитує дані з файлу та повертає список словників
+        """
+        data = []
+        try:
+            with open(self.file_path, 'r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if len(row) != 3:
+                        continue
+                    name, date, price = row
+                    try:
+                        date = datetime.strptime(date, "%Y-%m-%d")
+                        price = float(price)
+                        data.append({"name": name, "date": date, "price": price})
+                    except ValueError:
+                        continue
+        except FileNotFoundError:
+            print("Файл не знайдено")
+        return data
+
+
+
 if __name__ == "__main__":
     tracker = ProductPriceTracker("products.txt")
     product = input("Введіть назву товару: ")
